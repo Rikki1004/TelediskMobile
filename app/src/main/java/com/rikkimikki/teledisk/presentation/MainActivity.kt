8198e085361002c947172f9499f2d560d8e223c5
Daniil <rikki-mikki2@mail.ru>
2023-02-23 20:51:20 +0300

    add libTd

--- app/src/main/java/com/rikkimikki/teledisk/presentation/MainActivity.kt
+++ app/src/main/java/com/rikkimikki/teledisk/presentation/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.rikkimikki.teledisk.presentation
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.rikkimikki.teledisk.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +19,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //requestPermissions(this,123)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
     }
+
+
+
+    fun requestPermissions(activity: Activity, requestCode:Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setData(Uri.parse(String.format("package:%s", activity.getPackageName())));
+                activity.startActivityForResult(intent, requestCode);
+            } catch (e:Exception) {
+                val intent = Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                activity.startActivityForResult(intent, requestCode);
+            }
+        } else {
+            ActivityCompat.requestPermissions(activity,
+                arrayOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE),
+                requestCode)
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/rikkimikki/teledisk/presentation/MainFragment.kt
+++ app/src/main/java/com/rikkimikki/teledisk/presentation/MainFragment.kt
@@ -1,12 +1,18 @@
 package com.rikkimikki.teledisk.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import com.rikkimikki.teledisk.R
+import com.rikkimikki.teledisk.data.tdlib.Example
+import kotlin.concurrent.thread
 
 class MainFragment : Fragment() {
 
@@ -31,6 +37,15 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //val path = requireActivity().dataDir.absolutePath
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
+        println(path)
+        val a = Example(path+"/td/",path+"/td/tdlib.log")
+        //val a = Example("","logs/tdlib.log")
+        thread {
+            a.main()
+        }
+
     }
 
 }
